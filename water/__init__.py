c3d740219ca46beaa524d4ed042f2120cadb092e
feizhang2 <fei2zhang@gmail.com>
2016-04-16 14:22:45 +1000

    Update __init__.py
    Plugin design pattern research notes

--- water/__init__.py
+++ water/__init__.py
@@ -1,5 +1,10 @@
 """
-water detection algorithms: 
-from surface reflectance (observation NABR bands) and anciallary data (PQ, DSM, etc) 
-to pixel classification: water pixel or others.
+Purpose:  Water pixel detection algorithms: 
+Input:    Surface reflectance (observation NABR bands) and anciallary data (PQ, DSM, etc) 
+Output:   Pixel classification: water pixel or others.
+
+Use plugin architecture to facilitate the development and testing of different scientifc alogrithms.
+http://yapsy.sourceforge.net/#
+http://martyalchin.com/2008/jan/10/simple-plugin-framework/
+
 """
