60b20f6ffe2fd801302c3d649c6f69ce20b06213
FeiZhang <fei.zhang@ga.gov.au>
2016-04-19 18:54:57 +1000

    Updated the nc_tilestack_view.py to visualize bigger datasets

--- water/nc_tilestack_view.py
+++ water/nc_tilestack_view.py
@@ -6,16 +6,19 @@ import matplotlib.pyplot as plt
 from netCDF4 import Dataset, num2date, date2num
 from ipywidgets import *
 
-f = Dataset('/g/data/rs0/tiles/EPSG3577/LS5_TM_NBAR/LS5_TM_NBAR_3577_15_-40_2006.nc', 'r')
-#f = Dataset('/Softdata/data/water_extents/149_-036/py_stacked_CF.nc', 'r')
+#f = Dataset('/g/data/rs0/tiles/EPSG3577/LS5_TM_NBAR/LS5_TM_NBAR_3577_15_-40_2006.nc', 'r')
+f = Dataset('/Softdata/data/water_extents/149_-036_subset/py_stacked_CF.nc', 'r')
 
-band4view='band_2'
-bandarray = f[band4view][:, :, :]
+# The NC data variable name?
+band4view='Data' #'band_2'
+bandarray = f[band4view][:, :, :]  # this may use up memory error
+#bandarray = f[band4view]
 
 fig = plt.figure()
 
 time=0
-im = plt.imshow(bandarray[time,:,:], cmap=plt.get_cmap('viridis'), animated=True)
+#im = plt.imshow(bandarray[time,:,:], cmap=plt.get_cmap('viridis'), animated=True)
+im = plt.imshow(bandarray[time,:,:], cmap="Greys", animated=True)
 
 
 def updatefig(*args):
