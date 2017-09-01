e73dc318bd5cfffac1d63d2590cda5864122c92c
Ben Lewis <ben.lewis@ga.gov.au>
2017-09-02 01:28:08 +1000

    fuser working

--- wofs-summary/simple.py
+++ wofs-summary/simple.py
@@ -15,11 +15,21 @@ class reader:
         return netCDF4.Dataset(self.path)
     @property
     def metadata_doc(self):
-        return netcdf['dataset'][0].tostring().decode('unicode_escape')
+        return self.netcdf['dataset'][0].tostring().decode('unicode_escape')
     @property
     def metadata(self):
+        # This is slow (almost one second; quarter-second with C version)
         import yaml
-        return yaml.load(self.metadata_doc)
+        return yaml.load(self.metadata_doc, Loader=yaml.CLoader)
+    @property
+    def timestamp(self):
+        return self.metadata['extent']['center_dt']
+    @property
+    def date(self):
+        # TODO: consider bypass metadata, parse filename
+        import datetime
+        import dateutil.parser
+        return (dateutil.parser.parse(self.timestamp) + datetime.timedelta(hours=10)).date()
     @property
     def gqa(self):
         return self.metadata \
@@ -31,6 +41,21 @@ class reader:
     def water(self):
         return np.squeeze(self.netcdf['water'])
 
+class fuser:
+    def __init__(self, tiles):
+        self.tiles = tiles
+        print(len(tiles),end='')
+    @property
+    def water(self):
+        output = self.tiles[0].water
+        for tile in self.tiles[1:]:
+            # TODO: could numexpr
+            subsequent = tile.water
+            empty = (output & 1).astype(np.bool)
+            both = ~empty & ~((subsequent & 1).astype(np.bool))
+            output[empty] = subsequent[empty]
+            output[both] |= subsequent[both]
+        return output
 
 def do_work(observations): # read one file into memory at a time
     import numpy as np
@@ -48,7 +73,7 @@ def do_work(observations): # read one file into memory at a time
     print('')
     return wet_accumulator, dry_accumulator
 
-def produce_result(observations):
+def summarise_result(observations):
     wet, dry = do_work(observations)
 
     clear_observation_count = wet + dry
@@ -63,7 +88,40 @@ def produce_result(observations):
     ax2.imshow(frequency[::10,::10])
     #fig.show()
 
-import glob
-files = glob.glob(example_dir + '/*.nc')
+def get_observations(directory, maxtiles=None):
+    import pandas
+    import glob
+    files = glob.glob(directory + '/*.nc')[:maxtiles]
+    print('Opening')
+    rr = list(map(reader, files))
+    import time
+    t = time.clock()
+    print('Parsing dates of {} tiles'.format(len(files)))
+    p = [(r, r.date) for r in rr]
+    print(time.clock()-t)
+    print('Organising')
+    tiles = pandas.DataFrame(p,
+                             columns=['object', 'date'])
+    print('Grouping..')
+    g = tiles.groupby('date', sort=False)
+    return [fuser(list(obs.object)) for date, obs in g]
+    #for date, observation in g:
+    #    yield fuser(list(observation.object))
 
-produce_result([reader(f) for f in files[:20]])
+#print(reader(example_file).metadata.keys())
+
+#list(get_observations(example_dir, maxtiles=100))
+
+#summarise_result(get_observations(example_dir))
+
+z = get_observations(example_dir)
+summarise_result(z)
+
+def show_obs(fus):
+    import matplotlib.pyplot as plt
+    fig, axes = plt.subplots(1,1+len(fus.tiles))
+    for ax, w in zip(axes, [t.water for t in fus.tiles]+[fus.water]):
+        ax.imshow((w & np.uint8(1))[::10,::10])
+
+
+
