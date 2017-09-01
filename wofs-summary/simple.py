9149040f6f77bc4000c08eaa4857dc5f99064722
Ben Lewis <ben.lewis@ga.gov.au>
2017-09-01 18:05:21 +1000

    class form

--- wofs-summary/simple.py
+++ wofs-summary/simple.py
@@ -3,27 +3,34 @@
 example_dir = '/g/data/v10/testing_ground/wofs_brl/output/LS8_OLI_WATER/13_-35'
 example_file = example_dir + '/LS8_OLI_WATER_3577_13_-35_20130519000352000000_v1502857924.nc'
 
-def read_metadata(file):
-    import netCDF4
-    netcdf = netCDF4.Dataset(file)
-    return netcdf['dataset'][0].tostring().decode('unicode_escape')
+import numpy as np
+import functools
 
-def get_ortho(file):
-    import yaml
-    # lineage->source_datasets->classifer
-    # Water extent: {(0:nbart -> 0:nbart -> level1:), 1: pqa_wofs, 2: DEM}
-    # NBART albers: {0: nbart (scene)}
-    return yaml.load(read_metadata(file)) \
-        ['lineage']['source_datasets']['0'] \
-        ['lineage']['source_datasets']['0'] \
-        ['lineage']['source_datasets']['level1'] \
-        ['gqa']['residual']['cep90'] # GQA value
+class reader:
+    def __init__(self, file):
+        self.path = file
+    @property
+    def netcdf(self):
+        import netCDF4
+        return netCDF4.Dataset(self.path)
+    @property
+    def metadata_doc(self):
+        return netcdf['dataset'][0].tostring().decode('unicode_escape')
+    @property
+    def metadata(self):
+        import yaml
+        return yaml.load(self.metadata_doc)
+    @property
+    def gqa(self):
+        return self.metadata \
+            ['lineage']['source_datasets']['0'] \
+            ['lineage']['source_datasets']['0'] \
+            ['lineage']['source_datasets']['level1'] \
+            ['gqa']['residual']['cep90'] # GQA value
+    @property
+    def water(self):
+        return np.squeeze(self.netcdf['water'])
 
-def get_water(file):
-    import netCDF4
-    import numpy as np
-    netcdf = netCDF4.Dataset(file)
-    return np.squeeze(netcdf['water'])
 
 def do_work(observations): # read one file into memory at a time
     import numpy as np
@@ -34,7 +41,7 @@ def do_work(observations): # read one file into memory at a time
 
     land_or_sea = ~np.uint8(4) # to mask out the marine flag
     for f in observations:
-        bitfield = get_water(f) & land_or_sea
+        bitfield = f.water & land_or_sea
         wet_accumulator += bitfield == 128
         dry_accumulator += bitfield == 0
         print('.', end='')
@@ -59,4 +66,4 @@ def produce_result(observations):
 import glob
 files = glob.glob(example_dir + '/*.nc')
 
-produce_result(files[:10])
+produce_result([reader(f) for f in files[:20]])
