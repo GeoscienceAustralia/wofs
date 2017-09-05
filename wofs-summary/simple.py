331db8e17dc3819c4b9da4d88986025dc1d71106
Ben Lewis <ben.lewis@ga.gov.au>
2017-09-05 10:21:54 +1000

    generally usable (cmdline args etc)

--- wofs-summary/simple.py
+++ wofs-summary/simple.py
@@ -7,8 +7,16 @@ import numpy as np
 import functools
 
 class reader:
+    cell = None
     def __init__(self, file):
         self.path = file
+        if self.cell is None:
+            self.configure_cell(file)
+    @classmethod
+    def configure_cell(cls, example):
+        import rasterio
+        with rasterio.open('NetCDF:' + example + ':water') as f:
+            cls.cell = dict(affine=f.profile['affine'], crs=f.profile['crs'])
     @property
     def netcdf(self):
         import netCDF4
@@ -17,8 +25,11 @@ class reader:
     def metadata_doc(self):
         return self.netcdf['dataset'][0].tostring().decode('unicode_escape')
     @property
+    @functools.lru_cache()
     def metadata(self):
         # This is slow (almost one second; quarter-second with C version)
+        # TODO: consider querying database rather than file.
+        # i.e. get JSON, which is faster to parse.
         import yaml
         return yaml.load(self.metadata_doc, Loader=yaml.CLoader)
     @property
@@ -26,17 +37,22 @@ class reader:
         return self.metadata['extent']['center_dt']
     @property
     def date(self):
-        # TODO: consider bypass metadata, parse filename
         import datetime
         import dateutil.parser
         return (dateutil.parser.parse(self.timestamp) + datetime.timedelta(hours=10)).date()
     @property
     def gqa(self):
-        return self.metadata \
-            ['lineage']['source_datasets']['0'] \
+        import math
+        offset = self.metadata['lineage']['source_datasets']['0'] \
             ['lineage']['source_datasets']['0'] \
             ['lineage']['source_datasets']['level1'] \
-            ['gqa']['residual']['cep90'] # GQA value
+            ['gqa']['residual']['iterative_mean']
+        return math.hypot(float(offset['x']), float(offset['y']))
+        #return self.metadata \
+        #    ['lineage']['source_datasets']['0'] \
+        #    ['lineage']['source_datasets']['0'] \
+        #    ['lineage']['source_datasets']['level1'] \
+        #    ['gqa']['residual']['cep90']
     @property
     def water(self):
         return np.squeeze(self.netcdf['water'])
@@ -73,7 +89,7 @@ def do_work(observations): # read one file into memory at a time
     #print('')
     return wet_accumulator, dry_accumulator
 
-def summarise_result(observations):
+def summarise_result(observations, prefix=''):
     wet, dry = do_work(observations)
 
     clear_observation_count = wet + dry
@@ -82,35 +98,33 @@ def summarise_result(observations):
     with np.errstate(invalid='ignore'): # denominator may be zero
         frequency = wet / clear_observation_count
 
-    write('clear.tif', clear_observation_count)
-    write('wet.tif', wet)
-    write('frequency.tif', frequency.astype(np.float32), nodata=np.nan)
+    write(prefix + 'clear.tif', clear_observation_count)
+    write(prefix + 'wet.tif', wet)
+    write(prefix + 'frequency.tif', frequency.astype(np.float32), nodata=np.nan)
 
+    """
     import matplotlib.pyplot as plt
     fig, (ax1,ax2) = plt.subplots(1,2)
     ax1.imshow(clear_observation_count[::10,::10])
     ax2.imshow(frequency[::10,::10])
-    #fig.show()
+    #fig.show()"""
 
-def get_observations(directory, maxtiles=None):
+def get_filenames(*directories):
+    from glob import glob
+    return [x for d in directories for x in glob(d + '/*.nc')]
+
+def get_observations(files):
     import pandas
-    import glob
-    files = glob.glob(directory + '/*.nc')[:maxtiles]
-    #print('Opening')
-    rr = list(map(reader, files))
-    import time
-    t = time.clock()
-    #print('Parsing dates of {} tiles'.format(len(files)))
-    p = [(r, r.date) for r in rr]
-    #print(time.clock()-t)
-    #print('Organising')
-    tiles = pandas.DataFrame(p,
-                             columns=['object', 'date'])
-    #print('Grouping..')
+
+    rr = map(reader, files)
+
+    p = [(r, r.date) for r in rr if r.gqa<1] # GQA filter threshold
+    tiles = pandas.DataFrame(p, columns=['object', 'date'])
     g = tiles.groupby('date', sort=False)
+
+    print('{} observations; {} of {} tiles'.format(len(g), len(p), len(files)))
+
     return [fuser(list(obs.object)) for date, obs in g]
-    #for date, observation in g:
-    #    yield fuser(list(observation.object))
 
 
 
@@ -122,14 +136,18 @@ def show_obs(fus):
 
 def write(filename, data, nodata=None):
     import rasterio
-    with rasterio.open('NetCDF:'+example_file+':water') as example:
-        with rasterio.open(filename, mode='w', width=4000, height=4000,
-                           crs=example.profile['crs'],
-                           affine=example.profile['affine'],
-                           count=1, dtype=data.dtype.name,
-                           driver='GTIFF', nodata=nodata) as destination:
+    with rasterio.open(filename,
+                       mode='w',
+                       width=4000,
+                       height=4000,
+                       count=1,
+                       dtype=data.dtype.name,
+                       driver='GTIFF',
+                       nodata=nodata,
+                       tiled=True,
+                       compress='DEFLATE',
+                       **reader.cell) as destination:
             destination.write(data, 1)
-            destination.close()
 
 #print(reader(example_file).metadata.keys())
 
@@ -137,8 +155,21 @@ def write(filename, data, nodata=None):
 
 #summarise_result(get_observations(example_dir))s
 
-z = get_observations(example_dir)#, maxtiles=5)
-summarise_result(z)
+#z = get_observations(example_dir)#, maxtiles=5)
+#summarise_result(z)
 
 
+def main():
+    import sys
+    if len(sys.argv) < 3:
+        print("Usage: python simple.py  OUTPUT_PREFIX  INPUT_TILE_DIRS..")
+        sys.exit(2)
 
+    output_prefix = sys.argv[1]
+    input_dirs = sys.argv[2:]
+
+    obs = get_observations(get_filenames(*input_dirs))
+    summarise_result(obs, prefix=output_prefix)
+
+if __name__ == '__main__':
+    main()
