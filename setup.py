25bc8b4a34adc7ec85439c1cc6927a11d7f7e9d9
Damien Ayers <damien@omad.net>
2019-10-17 11:11:28 +1100

    Prep for building and testing on travis

--- setup.py
+++ setup.py
@@ -23,38 +23,20 @@ def _read(*parts):
         return fp.read()
 
 
-def _find_version(*file_paths):
-    version_file = _read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
 
-
-setup(
-    name='wofs',
-    version=_find_version("wofs", "__init__.py"),
-    cmdclass={'sdist': sdist},
-    url='https://github.com/GeoscienceAustralia/wofs',
-    description='Water Observations from Space - Digital Earth Australia',
-    long_description=open('README.rst', 'rt').read(),
-    author='Geoscience Australia',
-    author_email='damien.ayers@ga.gov.au',
-    maintainer='Geoscience Australia',
-    maintainer_email='',
-    license='Apache License 2.0',
-    packages=find_packages(),
-    package_data={
-        '': ['*.yaml', '*/*.yaml'],
-    },
-    include_package_data=True,
-    scripts=[],
-    setup_requires=[
-        'pytest-runner'
-    ],
-    data_files=[('wofs/config/', config_files)],
-    install_requires=[
+setup(name='wofs',
+      description='Water Observations from Space - Digital Earth Australia',
+      long_description=open('README.rst', 'r').read(),
+      license='Apache License 2.0',
+      url='https://github.com/GeoscienceAustralia/wofs',
+      author='Geoscience Australia',
+      maintainer='Geoscience Australia',
+      maintainer_email='',
+      packages=find_packages(),
+      data_files=[('wofs/config', config_files)],
+      use_scm_version=True,
+      setup_requires=['setuptools_scm'],
+      install_requires=[
           'datacube',
     ],
     tests_require=tests_require,
