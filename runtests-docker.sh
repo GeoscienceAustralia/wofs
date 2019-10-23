541b3b1048e5ca8607543e52a77177de6826f975
Damien Ayers <damien@omad.net>
2019-10-23 23:23:25 +0000

    Use Setuptools, not distutils
    It supports setuptools_scm (it's in the name). Means we get git based version numbers, instead of 0.0.0.

--- runtests-docker.sh
+++ runtests-docker.sh
@@ -2,4 +2,5 @@
 
 #docker pull opendatacube/datacube-core
 
-docker run --entrypoint "" --rm -v $PWD:/tmp/wofs -w /tmp/wofs opendatacube/datacube-core:latest /bin/sh -c "python3 setup.py sdist bdist_wheel && pip install -e .[terrain] && ./check-code.sh"
+docker run --entrypoint "" --user `id -u`:`id -g` --rm -v $PWD:/tmp/wofs -w /tmp/wofs \
+opendatacube/datacube-core:latest /bin/sh -c "python3 setup.py sdist bdist_wheel && pip install -e .[terrain] && ./check-code.sh"
