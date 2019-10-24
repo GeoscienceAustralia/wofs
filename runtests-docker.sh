16fe59eb824f1eee7c584110e86a36e603459c65
Damien Ayers <damien@omad.net>
2019-10-24 00:27:18 +0000

    Continue running docker as root (for now)

--- runtests-docker.sh
+++ runtests-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-#docker pull opendatacube/datacube-core
+docker pull opendatacube/datacube-core
 
-docker run --entrypoint "" --user `id -u`:`id -g` --rm -v $PWD:/tmp/wofs -w /tmp/wofs \
+# Null out the entrypoint since we aren't (yet) connecting to a database
+docker run --entrypoint "" --rm -v $PWD:/tmp/wofs -w /tmp/wofs \
 opendatacube/datacube-core:latest /bin/sh -c "python3 setup.py sdist bdist_wheel && pip install -e .[terrain] && ./check-code.sh"
