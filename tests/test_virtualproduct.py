823cb6abbe290919804a9a872715b6f5a044b683
Damien Ayers <damien@omad.net>
2020-08-23 17:41:00 +1000

    Clean up the test a little bit

--- tests/test_virtualproduct.py
+++ tests/test_virtualproduct.py
@@ -1,35 +1,30 @@
-import pytest
-from datetime import datetime
-import xarray as xr
-from datacube.virtual import Transformation, Measurement, construct, DEFAULT_RESOLVER
-import datacube
 from pathlib import Path
+
+import xarray as xr
+import yaml
+from datacube.virtual import construct
 
 from wofs.virtualproduct import WOfSClassifier
 
-import yaml
-
-from wofs.vp_wofs import woffles_ard_no_terrain_filter
 
 def test_virtualproduct():
-    data = xr.open_dataset(Path(__file__).parent / 'sample_c3_sr.nc', mask_and_scale=False)
-    data = data.rename({'oa_fmask': 'fmask'})
-    # data = data.expand_dims({'time': [datetime.now()]})
-    # data.attrs['crs'] = datacube.utils.geometry.CRS('EPSG:32754')
-    data.attrs['crs'] = 'EPSG:32754'
+    # Load sample surface reflectance data
+    sr_data = xr.open_dataset(Path(__file__).parent / 'sample_c3_sr.nc', mask_and_scale=False)
+    # and munge to make look more like data loaded by ODC
+    sr_data = sr_data.rename({'oa_fmask': 'fmask'})
+    sr_data.attrs['crs'] = 'EPSG:32754'
+    del sr_data.coords['band']
 
-    for dv in data.data_vars.values():
+    for dv in sr_data.data_vars.values():
         dv.attrs['nodata'] = dv.attrs['nodatavals']
-    # ds['nbart_blue'].attrs['nodata'] = ds['nbart_blue'].attrs['nodatavals']
 
     transform = WOfSClassifier()
-    wofl = transform.compute(data)
-
-    # wofl.to_netcdf(Path(__file__).parent / 'sample_wofl.nc')
+    wofl = transform.compute(sr_data)
 
     sample = xr.open_dataset(Path(__file__).parent / 'sample_wofl.nc', mask_and_scale=False)
 
     assert sample.equals(wofl)
+
 
 def foo():
     # measurements: [green, red, nir, swir1, swir2]
@@ -46,14 +41,12 @@ def foo():
     # db_port: 6432
     # db_database: odc_conf_test
 
-
     # [ard_interop] - collection upgrade DB
     # db_hostname: agdcstaging-db.nci.org.au
     # db_port:     6432
     # db_database: ard_interop
 
     dc = datacube.Datacube(env="odc_conf_test")
-
 
     vdbag = virtual_product.query(dc=dc, id='be43b7ce-c421-4c16-826d-a508f3e3d984')
 
