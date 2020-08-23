249f8be7e0663c1433926d99af760fcc7d328d3d
Damien Ayers <damien@omad.net>
2020-08-23 17:42:39 +1000

    Clean up test data gen a bit

--- tests/create_test_input_data.py
+++ tests/create_test_input_data.py
@@ -1,33 +1,39 @@
-import xarray as xr
-
-import yaml
 import sys
 from pathlib import Path
 
-in_dir = Path(sys.argv[1])
-out_file = sys.argv[2]
+import xarray as xr
+import yaml
 
-fname = list(in_dir.glob('*.odc-metadata.yaml'))[0]
 
-with open(fname) as fin:
-    doc = yaml.safe_load(fin)
+def create_sample_file():
+    in_dir = Path(sys.argv[1])
+    out_file = sys.argv[2]
 
-to_load = [(name, in_dir / details['path'])
-           for name, details in doc['measurements'].items()
-           if name.startswith('nbart') or 'fmask' in name]
-to_load = sorted(to_load, key=lambda tup: tup[1])
+    fname = list(in_dir.glob('*.odc-metadata.yaml'))[0]
 
-def load_band(fname):
-    data = xr.open_rasterio(fname)
-    return data.squeeze().sel(x=slice(None, None, 200), y=slice(None, None, 200))
+    with open(fname) as fin:
+        doc = yaml.safe_load(fin)
 
-data_vars = {name: load_band(fname) for name, fname in to_load}
+    to_load = [(name, in_dir / details['path'])
+               for name, details in doc['measurements'].items()
+               if name.startswith('nbart') or 'fmask' in name]
+    to_load = sorted(to_load, key=lambda tup: tup[1])
 
-ds = xr.Dataset(data_vars)
+    def load_band(fname):
+        data = xr.open_rasterio(fname)
+        return data.squeeze().sel(x=slice(None, None, 200), y=slice(None, None, 200))
 
-for dv in ds.data_vars.values():
-    dv.attrs['nodata'] = dv.attrs['nodatavals']
+    data_vars = {name: load_band(fname) for name, fname in to_load}
 
-ds = ds.expand_dims({'time': [doc['properties']['datetime'].replace(tzinfo=None)]})
+    ds = xr.Dataset(data_vars)
 
-ds.to_netcdf(out_file)
+    for dv in ds.data_vars.values():
+        dv.attrs['nodata'] = dv.attrs['nodatavals']
+
+    ds = ds.expand_dims({'time': [doc['properties']['datetime'].replace(tzinfo=None)]})
+
+    ds.to_netcdf(out_file)
+
+
+if __name__ == '__main__':
+    create_sample_file()
