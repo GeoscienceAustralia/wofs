efd2fc2560b4c86205d15d8a3be3e3715a50b82a
eloise-b <elly.birchall@gmail.com>
2021-04-19 05:34:15 +0000

    return wofls

--- wofs/wofls.py
+++ wofs/wofls.py
@@ -44,8 +44,8 @@ def woffles_ard(ard, dsm):
     nbar_bands = spectral_bands(ard)
     water = classifier.classify(nbar_bands) \
         | eo_filter(ard) \
-        | pq_filter(ard.pixel_quality)
-        #| fmask_filter(ard.fmask)
+        #| pq_filter(ard.pixel_quality)
+        | fmask_filter(ard.fmask)
 
     if dsm is not None:
         # terrain_filter arbitrarily expects a band named 'blue'
